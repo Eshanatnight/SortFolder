174f48e5e9b5add4a1c579d0644499e92c5430fe
Eshan Chatterjee <eshan2001221@gmail.com>
2021-11-11 14:23:45 +0530

    update to the main for testing prelims

--- src/CPP/main.cpp
+++ src/CPP/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
-
 #include <filesystem>
+#include "Directory Handler/directory_handler.h"
+#define SORTFOLDER "SortFolder.exe"
 
 int main(int argc, char** argv)
 {
+    DirectoryHandler object("C:/dev/SortFolder");
+    object.getObjects();
+
     if (argc < 2)
     {
-        std::cout << "Usage: " << argv[0] << " <path>" << std::endl;
+        std::cout << "Usage: " << SORTFOLDER << " <path>" << std::endl;
         return EXIT_FAILURE;
     }
 
